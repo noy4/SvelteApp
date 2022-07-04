4a106eeacd25a526770bbc261fd992fdec4aa011
noy4 <naoyan418@gmail.com>
2022-07-05 01:57:52 +0900

    buy ui

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
 
-<header class="navbar shadow">
+<header class="navbar shadow bg-base-100">
 	<button class="btn btn-ghost text-xl normal-case">Kuwa Coin</button>
 </header>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	const rate = 310_421
 </script>
 
 <svelte:head>
@@ -6,4 +7,46 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section class="flex flex-col justify-center items-center" />
+<section class="flex flex-col items-center mx-4">
+	<h3 class="mt-2">You have</h3>
+	<div>
+		<span class="text-5xl font-semibold">{(5000).toLocaleString()}</span>
+		<span class="font-bold">KWC</span>
+	</div>
+
+	<div class="card w-full max-w-sm bg-base-100 shadow-xl not-prose mt-8">
+		<div class="card-body items-center">
+			<h2 class="card-title">Buy 5,000 KWC</h2>
+			<div class="flex w-full justify-between">
+				<p>Rate</p>
+				<p class="flex-grow-0">{rate.toLocaleString()} KWC/ETH</p>
+			</div>
+			<div class="flex w-full justify-between">
+				<p>Price</p>
+				<p class="flex-grow-0">{5000 / rate} ETH</p>
+			</div>
+			<div class="card-actions mt-4">
+				<button class="btn btn-primary">Buy Now</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-full max-w-sm bg-base-100 shadow-xl not-prose mt-8">
+		<div class="card-body items-center">
+			<h2 class="card-title">Sell 5,000 KWC</h2>
+			<div class="flex w-full justify-between">
+				<p>Rate</p>
+				<p class="flex-grow-0">{rate.toLocaleString()} KWC/ETH</p>
+			</div>
+			<div class="flex w-full justify-between">
+				<p>Price</p>
+				<p class="flex-grow-0">{5000 / rate} ETH</p>
+			</div>
+			<div class="card-actions mt-4">
+				<button class="btn btn-primary">Sell Now</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="h-16" />
+</section>
