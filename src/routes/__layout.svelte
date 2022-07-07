59c77ac83897caaf24572bc73843a96d207d89a0
noy4 <naoyan418@gmail.com>
2022-07-08 03:53:18 +0900

    dayjs.fromNow()

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,7 @@
   import { BottomNav, Header } from '$components'
   import { Buffer } from 'buffer'
   import '$lib/styles/app.css'
+  import '$lib/utils/dayjs'
 
   window.Buffer = Buffer
 </script>
