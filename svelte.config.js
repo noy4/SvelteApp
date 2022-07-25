ff1060b4158c50b1993d9663f631f5d8cea076e0
noy4 <naoyan418@gmail.com>
2022-07-25 19:51:48 +0900

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -29,6 +29,7 @@ const config = {
     prerender: {
       default: true,
     },
+    trailingSlash: 'always',
   },
 }
 
