f21fd4db0b32d53396989daf4b19cb3a1302f0d2
noy4 <naoyan418@gmail.com>
2022-07-28 18:38:52 +0900

    home: down arrow

--- src/routes/index@root.svelte
+++ src/routes/index@root.svelte
@@ -38,6 +38,12 @@
         <button class="btn btn-primary not-prose"> Get it now </button>
       </a>
     </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1024 1024"
+      class="absolute bottom-0 w-12 h-12 fill-current animate-bounce"
+      ><path
+        d="M831.872 340.864L512 652.672L192.128 340.864a30.592 30.592 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.592 30.592 0 0 0-42.752 0z" /></svg>
   </div>
 </div>
 
